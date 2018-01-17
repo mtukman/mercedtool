83d072e537a72de7d9afcf8b207bbedc80683666
Mark Tukman <mark@tukmangeospatial.net>
2018-01-17 15:07:47 -0800

    changes

--- Reporting.py
+++ Reporting.py
@@ -1,384 +1,124 @@
-#-------------------------------------------------------------------------------
-#
-# Author:      Mark Tukman
-#
-# Created:     10/07/2014
-# Copyright:   (c) Tukman Geospatial LLC 2014
-# Unauthorized copying of this file, via any medium is strictly prohibited
-#
-# Proprietary and confidential
-#
-# Written by Mark Tukman (mark@tukmangeospatial.com)
-# If you'd like to use this code to support land conservation or
-# do something else cool, contact me - maybe I can help
-#-------------------------------------------------------------------------------
-def create_reports (treatments, conserved_acreage, output_folder, vineyard_count, rr_count):
-#for unit testing
-#def create_reports():
-    import Generic
-
-
-    layers_to_delete_at_finish=["CARB_CONV", "CMB67"]
-    #set title font size
-    tsize = 11
-    #comment out the below 6 lines when not debugging
-    #Generic.set_paths_and_workspaces("D:/temp/scratch.gdb", "D:/CLOUD/Shared/Open Space/", 10000, 10000, "None", "", 'Carbon Framework/GIS Data/MASTER_DATA/', "D:/temp/scratch.gdb", "PLACKARD" )
-    #treatments = (["Preservation Ranch Vineyards",Generic.PresRanchVineyards], ["Preservation Ranch Estates",Generic.PresRanchEstates])
-    #conserved_acreage = "120K Acres Conserved Next 20 Yrs."
-    #output_folder = "D:/temp/"
-    #vineyard_count = 10
-    #rr_count = 6
-
-    Generic.add_to_logfile("Creating reports...")
-    import numpy as np
-    import os
-    import arcpy
-
-
-    arcpy.env.workspace = Generic.ArcWorkspace
-    arcpy.env.scratchWorkspace = Generic.ArcScratchWorkspace
-    #arcpy.env.scratchFolder = ArcScratchFolder
-    #arcpy.env.scratchGDB= ArcScratchGDB
-
-    arcpy.CheckOutExtension("Spatial")
-    import Generic
-    import matplotlib.pyplot as plt
-    import matplotlib.pyplot as plt2
-    from matplotlib.backends.backend_pdf import PdfPages
-
-    arcpy.env.extent = Generic.MASK
-    #arcpy.env.mask = Generic.MASK
-    arcpy.env.cellSize = Generic.LANDFIRE
-    arcpy.env.snapRaster = Generic.LANDFIRE
-
-
-    if os.path.isfile(os.path.join(output_folder, 'reports.pdf')):
-        os.remove(os.path.join(output_folder, 'reports.pdf'))
-    pp = PdfPages(os.path.join(output_folder, 'reports.pdf'))
-
-    carbon_raster = Generic.CarbonRaster
-    table_carbon = Generic.CarbonStats
-
-
-    def create_dict_of_vals(input_raster_list_for_combine, combined_thematic_raster,   input_vector_for_join, thematic_vector_attribute, thematic_ras_joinfield = "OBJECTID" ):
-        # Replace a layer/table view name with a path to a dataset (which can be a layer ) or create the layer/table view within the script
-        # The following inputs are layers or table views: "CB_CW", "Calwater_TA"
-        cmb1= arcpy.sa.Combine(input_raster_list_for_combine)
-        cmb1.save("CMB67")
-        arcpy.MakeTableView_management("CMB67" , "cw")
-        arcpy.AddJoin_management(in_layer_or_view="cw",in_field=os.path.basename(combined_thematic_raster)[0:26],join_table=input_vector_for_join,join_field=thematic_ras_joinfield,join_type="KEEP_ALL")
-        arcpy.AddJoin_management(in_layer_or_view="cw",in_field="CARB_CONV",join_table=carbon_raster,join_field="Value",join_type="KEEP_ALL")
-
-        urb_per={}
-        vineyard_per={}
-
-        fields = []
-        fields.append(os.path.basename(input_vector_for_join) + "." + thematic_vector_attribute)
-        fields.append("VAT_CMB67." + "Count")
-        fields.append("VAT_CMB67." + "Value")
-
-        for i in ("151", "19"):
-            if i == '151': ref = vineyard_per
-            else: ref= urb_per
-            where= "VAT_" + os.path.basename(carbon_raster)+ "." + "LU_30 = " + i + " and VAT_" + os.path.basename(carbon_raster)+ "." + "LU_10 <> " + i
-            sc = arcpy.da.SearchCursor("cw", fields, where)
-            count = 0
-            for row in sc:
-                if row[0] in ref.keys():
-                    ref[row[0]] = ref[row[0]] + (row[1]*900)
-                else:
-                    ref[row[0]] = (row[1]*900)
-            del sc
-
-        return(vineyard_per, urb_per)
-
-
-    dict_carbon_overall={}
-
-    title = conserved_acreage
-
-    c=1
-    tm=""
-    if len(treatments) >0 and c<len(treatments):
-        for i in treatments:
-            tm = tm + "Treatment " + str(c) + ": " + i[0] + "\n"
-            c= c+1
-    elif len(treatments) >0 and c==len(treatments):
-        for i in treatments:
-            tm = tm + "Treatment " + str(c) + ": " + i[0]
-            c= c+1
-
-    if len(treatments) >1:
-        title = title + "\n" + tm
-
-    def add_to_dict(dict_name, line_title, table, bau=1):
-        vals = []
-        fields_carbon = ["SUM_Tot_C_90", "SUM_Tot_C_10", "SUM_Tot_C_30",  "SUM_Tot_C_30BAU", "SUM_Tot_C_50",  "SUM_Tot_C_50BAU" ]
-        for row in arcpy.da.SearchCursor(table, (fields_carbon)):
-            if bau == 1:
-                vals.append(row[0])
-                vals.append(row[1])
-                vals.append(row[3])
-                vals.append(row[5])
-            else:
-                vals.append(row[0])
-                vals.append(row[1])
-                vals.append(row[2])
-                vals.append(row[4])
-
-        dict_name[line_title] = vals
-
-    treatment_name_bau = "Business as Usual; Vineyard acreage cap: " + str(Generic.acreage_cap_vineyard) + ", Ruralres acreage cap: " + str(Generic.acreage_cap_urban)
-    treatment_name = tm
-
-    add_to_dict(dict_carbon_overall, treatment_name_bau, table_carbon, 1)
-    if len(treatments) >0:
-        add_to_dict(dict_carbon_overall, treatment_name, table_carbon, 0)
-
-    years = [1990, 2010, 2030, 2050]
-    colordict={"Business as Usual":"red","green""Improved Conifer Management":"green", "Fuels Management - Mayacamas":"yellow", "No Treatment":"black", \
-    "40K Acres Conserved Next 20 Yrs.":"red", "80K Acres Conserved Next 20 Yrs.":"yellow", "120K Acres Conserved Next 20 Yrs.":"green" }
-    colordict[treatment_name]="yellow"
-    colordict[treatment_name_bau]="red"
-
-    fig = plt.figure(figsize=(8.5, 11))
-    for i in reversed(sorted(dict_carbon_overall.keys())):
-
-        plt.subplot(3,1, 1)
-        plt.plot(years, dict_carbon_overall[i], label=i, color=colordict[i])
-
-    plt.xlabel('Year')
-    plt.ylabel('Metric Tons Carbon')
-    plt.legend(loc=2, prop={'size':8})
-    plt.tight_layout()
-    plt.title(title)
-
-    if (rr_count>0 or vineyard_count>0):
-        #first create just a raster of the vineyards and rr areas, so that the combine works
-        conversion_only_ras = arcpy.sa.Con(carbon_raster, carbon_raster,"#", "LU_30 in (19, 151)")
-        conversion_only_ras.save(os.path.join(Generic.ArcWorkspace, "CARB_CONV"))
-        #BEGIN BAR CHART THAT SHOWS TOTAL ACRES OF VINEYARD AND RR BY WATERSHED
-        vineyard_per_shed, urb_per_shed = create_dict_of_vals([os.path.join(Generic.ArcWorkspace, "CARB_CONV"), Generic.Calwater_R], Generic.Calwater_R, Generic.Calwater, "HANAME")
-
-        for i in vineyard_per_shed.keys():
-            if i not in urb_per_shed.keys():
-                urb_per_shed[i] = 0
-
-        for i in urb_per_shed.keys():
-            if i not in vineyard_per_shed.keys():
-                vineyard_per_shed[i] = 0
-
-
-        x = [i for i in sorted(vineyard_per_shed.keys())]
-        y = []
-        k= []
-        for i in x:
-            y.append(vineyard_per_shed[i]/4046.86)
-            k.append(urb_per_shed[i]/4046.86)
-        ax= plt.subplot(3,1, 2)
-        n = len(vineyard_per_shed)
-        ind = np.arange(n)
-        w = 0.3
-        r1= ax.bar(ind, y,width=w,color='b',align='center')
-        r2=ax.bar(ind+w, k,width=w,color='g',align='center')
-        #ax.xaxis_date()
-        ax.autoscale(tight=True)
-        ax.set_title('Projected Vineyard and Rural Residential Development by Watershed', size=tsize)
-        ax.set_xticks(ind + (.5*w))
-        ax.set_ylabel('Acres')
-        xTickMarks = x
-        xtickNames = ax.set_xticklabels(xTickMarks, )
-        plt.setp(xtickNames, rotation=40, ha='center', fontsize=8)
-        ymax = Generic.ylim(max(vineyard_per_shed.values())/4047, max(urb_per_shed.values())/4047)
-        plt.ylim(0, ymax)
-
-        ax.legend((r1[0], r2[0]), ('Vineyard Conversion','Rural Residential Expansion'), prop={'size':8}, ncol=2)
-        plt.tight_layout()
-        #END CHART
-
-
-        #BEGIN BAR CHART THAT SHOWS ACRES OF VINEYARD/RR EXPANSION INTO PRIME FARMLAND
-        vineyard_per_fmmp, urb_per_fmmp = create_dict_of_vals(["CARB_CONV", Generic.FMMP_R], Generic.FMMP_R, Generic.FMMP, "DESC_")
-
-        for i in vineyard_per_fmmp.keys():
-            if i not in urb_per_fmmp.keys():
-                urb_per_fmmp[i] = 0
-
-        for i in urb_per_fmmp.keys():
-            if i not in vineyard_per_fmmp.keys():
-                vineyard_per_fmmp[i] = 0
-
-        x = [i for i in sorted(vineyard_per_fmmp.keys())]
-
-        y = []
-        k= []
-        for i in x:
-            y.append(vineyard_per_fmmp[i]/4046.86)
-            k.append(urb_per_fmmp[i]/4046.86)
-        ax= plt.subplot(3,1, 3)
-
-        n = len(vineyard_per_fmmp)
-        ind = np.arange(n)
-        w = 0.3
-        r1= ax.bar(ind, y,width=w,color='b',align='center')
-        r2=ax.bar(ind+w, k,width=w,color='g',align='center')
-        #ax.xaxis_date()
-        ax.autoscale(tight=True)
-        ax.set_title('Projected Vineyard and Rural Residential Development by Farmland Type', size=tsize)
-        ax.set_xticks(ind + (.5*w))
-        ax.set_ylabel('Acres')
-        xTickMarks = x
-        xtickNames = ax.set_xticklabels(xTickMarks, )
-        plt.setp(xtickNames, rotation=40, fontsize=8, ha='center')
-
-        ymax = Generic.ylim(max(vineyard_per_fmmp.values())/4047, max(urb_per_fmmp.values())/4047)
-        plt.ylim(0, ymax)
-        ax.legend((r1[0], r2[0]), ('Vineyard Conversion','Rural Residential Expansion'), prop={'size':8}, ncol=2)
-        plt.tight_layout()
-
-        #END CHART
-
-        pp.savefig()
-
-        fig = plt.figure(figsize=(8.5, 11))
-
-     #BEGIN BAR CHART THAT SHOWS ACRES OF VINEYARD/RR EXPANSION IN the context of terrestrial habitat
-        vineyard_per_th, urb_per_th = create_dict_of_vals(["CARB_CONV", Generic.TerrestrialHabitat_Raster], Generic.TerrestrialHabitat_Raster, Generic.TerrestrialHab_Quint, "Rank", "Quintile")
-
-        for i in vineyard_per_th.keys():
-            if i not in urb_per_th.keys():
-                urb_per_th[i] = 0
-
-        for i in urb_per_th.keys():
-            if i not in vineyard_per_th.keys():
-                vineyard_per_th[i] = 0
-
-
-        myorder = ['Highest','Mid-High','Mid','Mid-Low','Lowest']
-        x=[]
-        for i in myorder:
-            if i in sorted(vineyard_per_th.keys()):
-                x.append(i)
-        y = []
-        k= []
-        for i in x:
-            y.append(vineyard_per_th[i]/4046.86)
-            k.append(urb_per_th[i]/4046.86)
-        ax= plt.subplot(3,1, 1)
-
-        n = len(vineyard_per_th)
-        ind = np.arange(n)
-        w = 0.3
-        r1= ax.bar(ind, y,width=w,color='b',align='center')
-        r2=ax.bar(ind+w, k,width=w,color='g',align='center')
-        #ax.xaxis_date()
-        ax.autoscale(tight=True)
-        ax.set_title('Projected Vineyard and Rural Residential Development by Terrestrial Habitat Value', size=tsize)
-        ax.set_xticks(ind + (.5*w))
-        ax.set_ylabel('Acres')
-        xTickMarks = x
-        xtickNames = ax.set_xticklabels(xTickMarks, )
-        plt.setp(xtickNames, rotation=40, fontsize=8, ha='center')
-
-        ymax = Generic.ylim(max(vineyard_per_th.values())/4047, max(urb_per_th.values())/4047)
-        plt.ylim(0, ymax)
-
-        ax.legend((r1[0], r2[0]), ('Vineyard Conversion','Rural Residential Expansion'), prop={'size':8}, ncol=2)
-        plt.tight_layout()
-
-    #BEGIN BAR CHART THAT SHOWS ACRES OF VINEYARD/RR EXPANSION IN the context of Groundwater Recharge
-        vineyard_per_gwr, urb_per_gwr = create_dict_of_vals(["CARB_CONV", Generic.GroundWaterRecharge_Raster], Generic.GroundWaterRecharge_Raster, Generic.GroundWaterRecharge_Quint, "Rank", "Quintile")
-
-        for i in vineyard_per_gwr.keys():
-            if i not in urb_per_gwr.keys():
-                urb_per_gwr[i] = 0
-
-        for i in urb_per_gwr.keys():
-            if i not in vineyard_per_gwr.keys():
-                vineyard_per_gwr[i] = 0
-
-
-        myorder = ['Highest','Mid-High','Mid','Mid-Low','Lowest']
-        x=[]
-        for i in myorder:
-            if i in sorted(vineyard_per_gwr.keys()):
-                x.append(i)
-        y = []
-        k= []
-        for i in x:
-            y.append(vineyard_per_gwr[i]/4046.86)
-            k.append(urb_per_gwr[i]/4046.86)
-        ax= plt.subplot(3,1, 2)
-
-        n = len(vineyard_per_gwr)
-        ind = np.arange(n)
-        w = 0.3
-        r1= ax.bar(ind, y,width=w,color='b',align='center')
-        r2=ax.bar(ind+w, k,width=w,color='g',align='center')
-        #ax.xaxis_date()
-        ax.autoscale(tight=True)
-        ax.set_title('Projected Vineyard and Rural Residential Development by Groundwater Recharge Value', size=tsize)
-        ax.set_xticks(ind + (.5*w))
-        ax.set_ylabel('Acres')
-        xTickMarks = x
-        xtickNames = ax.set_xticklabels(xTickMarks, )
-        plt.setp(xtickNames, rotation=40, fontsize=8, ha='center')
-
-        ymax = Generic.ylim(max(vineyard_per_gwr.values())/4047, max(urb_per_gwr.values())/4047)
-        plt.ylim(0, ymax)
-
-        ax.legend((r1[0], r2[0]), ('Vineyard Conversion','Rural Residential Expansion'), prop={'size':8}, ncol=2)
-        plt.tight_layout()
-
-    #BEGIN BAR CHART THAT SHOWS ACRES OF VINEYARD/RR EXPANSION IN the context of Water Yield
-        vineyard_per_wy, urb_per_wy = create_dict_of_vals(["CARB_CONV", Generic.WaterYield_Raster], Generic.WaterYield_Raster, Generic.WaterYield_Quint, "Rank", "Quintile")
-
-        for i in vineyard_per_wy.keys():
-            if i not in urb_per_wy.keys():
-                urb_per_wy[i] = 0
-
-        for i in urb_per_wy.keys():
-            if i not in vineyard_per_wy.keys():
-                vineyard_per_wy[i] = 0
-
-        myorder = ['Highest','Mid-High','Mid','Mid-Low','Lowest']
-        x=[]
-        for i in myorder:
-            if i in sorted(vineyard_per_wy.keys()):
-                x.append(i)
-        y = []
-        k= []
-        for i in x:
-            y.append(vineyard_per_wy[i]/4046.86)
-            k.append(urb_per_wy[i]/4046.86)
-        ax= plt.subplot(3,1, 3)
-
-        n = len(vineyard_per_wy)
-        ind = np.arange(n)
-
-        w = 0.3
-        r1= ax.bar(ind, y,width=w,color='b',align='center')
-        r2=ax.bar(ind+w, k,width=w,color='g',align='center')
-        #ax.xaxis_date()
-        ax.autoscale(tight=True)
-        ax.set_title('Projected Vineyard and Rural Residential Development by Water Yield', size=tsize)
-        ax.set_xticks(ind + (.5*w))
-        ax.set_ylabel('Acres')
-        xTickMarks = x
-        xtickNames = ax.set_xticklabels(xTickMarks, )
-        plt.setp(xtickNames, rotation=40, fontsize=8, ha='center')
-        ymax = Generic.ylim(max(vineyard_per_wy.values())/4047, max(urb_per_wy.values())/4047)
-        plt.ylim(0, ymax)
-
-        ax.legend((r1[0], r2[0]), ('Vineyard Conversion','Rural Residential Expansion'), prop={'size':8}, ncol=2)
-        plt.tight_layout()
-
-
-    pp.savefig()
-
-    pp.close()
-
-    for i in layers_to_delete_at_finish:
-        if arcpy.Exists(i):
-            arcpy.Delete_management(i)
-    #plt.show()
+import plotly.graph_objs as go
+import plotly.plotly as py
+
+import numpy as np
+import re
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import numpy as np
+
+
+import plotly.dashboard_objs as dashboard
+
+import IPython.display
+from IPython.display import Image
+
+def fileId_from_url(url):
+    """Return fileId from a url."""
+    raw_fileId = re.findall("~[A-z]+/[0-9]+", url)[0][1: ]
+    return raw_fileId.replace('/', ':')
+    
+def sharekey_from_url(url):
+    """Return the sharekey from a url."""
+    if 'share_key=' not in url:
+        return "This url is not 'sercret'. It does not have a secret key."
+    return url[url.find('share_key=') + len('share_key='):]
+
+my_dboard = dashboard.Dashboard()
+my_dboard.get_preview()
+
+x0 = np.random.randn(50)
+x1 = np.random.randn(50) + 2
+x2 = np.random.randn(50) + 4
+x3 = np.random.randn(50) + 6
+
+colors = ['#FAEE1C', '#F3558E', '#9C1DE7', '#581B98']
+
+trace0 = go.Box(x=x0, marker={'color': colors[0]})
+trace1 = go.Box(x=x1, marker={'color': colors[1]})
+trace2 = go.Box(x=x2, marker={'color': colors[2]})
+trace3 = go.Box(x=x3, marker={'color': colors[3]})
+data = [trace0, trace1, trace2, trace3]
+
+
+url_2 = py.plot(data, filename='box-plots-for-dashboard', sharing='secret', auto_open=True)
+py.iplot(data, filename='box-plots-for-dashboard')
+
+colorscale = [[0, '#FAEE1C'], [0.33, '#F3558E'], [0.66, '#9C1DE7'], [1, '#581B98']]
+trace1 = go.Scatter(
+    y = np.random.randn(500),
+    mode='markers',
+    marker=dict(
+        size='16',
+        color = np.random.randn(500),
+        colorscale=colorscale,
+        showscale=True
+    )
+)
+data = [trace1]
+url_1 = py.plot(data, filename='scatter-for-dashboard', auto_open=False)
+
+py.iplot(data, filename='scatter-for-dashboard')
+
+fileId_1 = fileId_from_url(url_1)
+fileId_2 = fileId_from_url(url_2)
+
+
+box_a = {
+    'type': 'box',
+    'boxType': 'plot',
+    'fileId': fileId_1,
+    'title': 'scatter-for-dashboard'
+}
+
+text_for_box = """ 
+## Distributions: 
+
+
+#### Scatter Plot
+1. Ranging 0 - 500
+2. Even distribution
+
+#### Box Plot
+1. Similar Range
+2. Outliers present in trace 1 and trace 3
+
+You can view more markdown tips [here](https://daringfireball.net/projects/markdown/syntax).
+"""
+
+box_b = {
+    'type': 'box',
+    'boxType': 'text',
+    'text': text_for_box,
+    'title': 'Markdown Options for Text Box'
+}
+
+box_c = {
+    'type': 'box',
+    'boxType': 'plot',
+    'fileId': fileId_2,
+    'title': 'box-for-dashboard',
+    'shareKey': sharekey_from_url(url_2)
+}
+
+my_dboard.insert(box_a)
+
+my_dboard.insert(box_b, 'above', 1)
+my_dboard.insert(box_c, 'left', 2)
+
+
+my_dboard['settings']['title'] = 'My First Dashboard with Python'
+my_dboard['settings']['logoUrl'] = 'https://images.plot.ly/language-icons/api-home/python-logo.png'
+
+my_dboard['settings']['links'] = []
+my_dboard['settings']['links'].append({'title': 'Link to Plotly', 'url': 'https://plot.ly/'})
+my_dboard['settings']['links'].append({'title': 'Link to Python Website', 'url': 'https://www.python.org/'})
+
+
+import plotly.plotly as py
+py.dashboard_ops.upload(my_dboard, 'My First Dashboard with Python')
+
+
+
